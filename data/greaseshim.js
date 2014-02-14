70fb2b12f3ad89c3b5310121d77ffa19f6c524a3
Kabir Sala <spittiepie@gmail.com>
2014-02-14 02:31:56 +0100

    Add support for GM_xmlhttpRequest

--- data/greaseshim.js
+++ data/greaseshim.js
@@ -11,7 +11,7 @@ function GM_getValue(_name, _default) {
 };
 
 function GM_deleteValue(_name) {
-  localStorage.removeItem(_name) 
+  localStorage.removeItem(_name);
 };
 
 function GM_setClipboard(_text) {
@@ -20,9 +20,19 @@ function GM_setClipboard(_text) {
 
 //Deprecated
 function GM_log(_message) {
-  console.log(_message)
+  console.log(_message);
 };
 
-self.port.on('load-userscript', function(script) {
-  eval(script);
+function GM_xmlhttpRequest(_details) {
+  //Ugly hack? Race condition? Memory leak?
+  _onload = _details.onload;
+  self.port.emit("GM_xmlhttpRequest", _details);
+};
+
+self.port.on("callback_GM_xmlhttpRequest", function(_response) {
+  _onload(_response);
+});
+
+self.port.on("load-userscript", function(_script) {
+  eval(_script);
 });
